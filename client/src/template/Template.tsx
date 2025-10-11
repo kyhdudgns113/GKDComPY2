3919a08651e59c134c9a7a5d870259a4bbc92e20
unknown <dudgns113@gmail.com>
2025-10-11 12:09:47 +0900

    25.10.11 Client: Header 기능 및 디자인 추가
    :construction: 클라이언트 사이트
      - :heavy_plus_sign: [Client] Template Header 디자인 추가
      - :heavy_plus_sign: [Client] Tempalte Header 유사 새로고침 추가
      - :heavy_plus_sign: [Client] Tempalte Header 로그아웃 버튼 추가
      - :heavy_plus_sign: [Client] Tempalte 토큰 자동확인 추가
    
    :white_large_square: 관리자 사이트
        - :white_large_square: [Admin] 공동체 설정 정보 수정 버튼
        - :white_large_square: [Admin] 클럽 정보 수정 기능
        - :white_large_square: [Admin] 공동체, 유저, 클럽 삭제 기능

--- client/src/template/Template.tsx
+++ client/src/template/Template.tsx
@@ -1,4 +1,7 @@
-import {Outlet} from 'react-router-dom'
+import {useEffect} from 'react'
+import {Outlet, useNavigate} from 'react-router-dom'
+
+import {useAuthCallbacksContext} from '@context'
 
 import {Footer} from './Footer'
 import {Header} from './Header'
@@ -12,6 +15,21 @@ import './_styles/Template.scss'
 type TemplateProps = DivCommonProps & {}
 
 export const Template: FC<TemplateProps> = ({className, style, ...props}) => {
+  const {refreshToken} = useAuthCallbacksContext()
+
+  const navigate = useNavigate()
+
+  // RefreshToken 실패하면 루트 페이지를 통해 로그인 페이지로 이동
+  useEffect(() => {
+    refreshToken() // ::
+      .then(res => {
+        if (!res) {
+          navigate('/')
+        }
+      })
+      .catch(() => navigate('/'))
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className={`Template ${className || ''}`} style={style} {...props}>
       {/* 1. 템플릿 헤더 */}
