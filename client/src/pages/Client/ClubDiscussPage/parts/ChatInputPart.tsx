5a646164d3469f50da9f24c3487f18b5bf99aaaa
unknown <dudgns113@gmail.com>
2025-10-14 14:07:18 +0900

    25.10.14 [Client] 회의록: 채팅방 입력 컴포넌트 추가 / 회의록 스타일 조정
    :art: [Client] 회의록: 문서 클릭시 배경색 변하는 transition 설정
    :art: [Client] 회의록: 버튼 hover 시 transition 조정
    :art: [Client] 회의록: 채팅방 배경 어둡게 변경
    
    :construction: 회의록 채팅 기능 추가
      - :heavy_plug_sign: [Client] 채팅 입력부분 컴포넌트 추가
    
    :construction: 클라이언트 사이트
      - :construction: Lefter 요소 채우기
    
    :white_large_square: 관리자 사이트
        - :white_large_square: [Admin] 공동체 설정 정보 수정 버튼
        - :white_large_square: [Admin] 클럽 정보 수정 기능
        - :white_large_square: [Admin] 공동체, 유저, 클럽 삭제 기능

--- client/src/pages/Client/ClubDiscussPage/parts/ChatInputPart.tsx
+++ client/src/pages/Client/ClubDiscussPage/parts/ChatInputPart.tsx
@@ -1,12 +1,19 @@
+import {Icon} from '@component'
+
 import type {FC} from 'react'
 import type {DivCommonProps} from '@prop'
+
+import '../_style/ChatInputPart.scss'
 
 type ChatInputPartProps = DivCommonProps & {}
 
 export const ChatInputPart: FC<ChatInputPartProps> = ({className, style, ...props}) => {
   return (
     <div className={`ChatInput_Part ${className || ''}`} style={style} {...props}>
-      ChatInputPart
+      <div className="_container_input">
+        <textarea className="_input_chat" />
+      </div>
+      <Icon iconName="send" className="_button_send" />
     </div>
   )
 }
