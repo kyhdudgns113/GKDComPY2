21e39fb3274a99b5ee57b32dfb5347abcc69a916
unknown <dudgns113@gmail.com>
2025-10-14 13:48:19 +0900

    25.10.14 [Client] ChatSlice: 채팅 배열 및 큐 관련 action 추가
    :construction: 회의록 채팅 기능 추가
      - :heavy_plug_sign: ChatSlice 채팅배열 및 큐 관련 action 추가
    
    :construction: 클라이언트 사이트
      - :construction: Lefter 요소 채우기
    
    :white_large_square: 관리자 사이트
        - :white_large_square: [Admin] 공동체 설정 정보 수정 버튼
        - :white_large_square: [Admin] 클럽 정보 수정 기능
        - :white_large_square: [Admin] 공동체, 유저, 클럽 삭제 기능

--- client/src/manager/contexts/chat/_effects.tsx
+++ client/src/manager/contexts/chat/_effects.tsx
@@ -1,4 +1,6 @@
-import {createContext, useContext} from 'react'
+import {createContext, useContext, useEffect} from 'react'
+
+import {useAppDispatch, useChatActions, useChatStates} from '@store'
 
 import type {FC, PropsWithChildren} from 'react'
 
@@ -10,6 +12,17 @@ export const ChatEffectsContext = createContext<ContextType>({})
 export const useChatEffectsContext = () => useContext(ChatEffectsContext)
 
 export const ChatEffectsProvider: FC<PropsWithChildren> = ({children}) => {
-  //
+  const {chatQueue} = useChatStates()
+  const {popChatQueueToArr} = useChatActions()
+
+  const dispatch = useAppDispatch()
+
+  // 채팅 큐에 채팅 있으면 배열로 넘겨줌
+  useEffect(() => {
+    if (chatQueue.length > 0) {
+      dispatch(popChatQueueToArr())
+    }
+  }, [chatQueue, dispatch]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return <ChatEffectsContext.Provider value={{}}>{children}</ChatEffectsContext.Provider>
 }
--- client/src/manager/stores/slices/ChatSlice.ts
+++ client/src/manager/stores/slices/ChatSlice.ts
@@ -7,11 +7,13 @@ import * as ST from '@shareType'
 // State 타입 정의
 interface ChatState {
   chatArr: ST.ChatType[]
+  chatQueue: ST.ChatType[]
 }
 
 // 초기 상태
 const initialState: ChatState = {
-  chatArr: []
+  chatArr: [],
+  chatQueue: []
 }
 
 // Slice 생성 (액션 + 리듀서를 한번에)
@@ -19,8 +21,26 @@ export const chatSlice = createSlice({
   name: 'chat',
   initialState,
   reducers: {
+    // 채팅 처음 불러올때
     setChatArr: (state, action: PayloadAction<ST.ChatType[]>) => {
       state.chatArr = action.payload
+    },
+    // 이전 채팅 불러올때
+    pushFrontChatArr: (state, action: PayloadAction<ST.ChatType[]>) => {
+      state.chatArr = [...action.payload, ...state.chatArr]
+    },
+    // ::
+    // 채팅 실시간으로 수신될때
+    pushChatQueue: (state, action: PayloadAction<ST.ChatType>) => {
+      state.chatQueue.push(action.payload)
+    },
+    // 채팅 큐에서 배열로 넘겨줄때
+    popChatQueueToArr: state => {
+      const chatQueue = state.chatQueue
+      if (chatQueue.length > 0) {
+        state.chatArr = [...chatQueue, ...state.chatArr]
+        state.chatQueue = []
+      }
     }
   }
 })
