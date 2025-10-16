3660f39252e2df416631c2b50a29c6a7c65757b6
unknown <dudgns113@gmail.com>
2025-10-17 03:43:07 +0900

    25.10.17 [Client] weekOId URL 파싱 및 관리 추가
    :construction: [Client] 대전 기록 페이지
      - :white_large_square: 주간 기록 테이블 파츠 컴포넌트
        - :heavy_plus_sign: [Redux] weekOId 관리 추가
        - :heavy_plus_sign: [Context] weekOId 관리 추가
    
    :white_large_square: [Client] 메인 페이지
      - :white_large_square: 공동체 멤버 수정 기능
    :white_large_square: [Client] 전체 멤버 페이지
    :white_large_square: [Client] 클럽 멤버 페이지
      - :white_large_square: 멤버 최근 기록 불러오기 기능

--- client/src/manager/stores/slices/RecordSlice.ts
+++ client/src/manager/stores/slices/RecordSlice.ts
@@ -6,11 +6,13 @@ import * as ST from '@shareType'
 
 // State 타입 정의
 interface RecordState {
+  weekOIdOpened: string
   weekRowArr: ST.WeekRowType[]
 }
 
 // 초기 상태
 const initialState: RecordState = {
+  weekOIdOpened: '',
   weekRowArr: []
 }
 
@@ -19,6 +21,16 @@ export const recordSlice = createSlice({
   name: 'record',
   initialState,
   reducers: {
+    // 주차 OId 해제
+    resetWeekOIdOpened: state => {
+      state.weekOIdOpened = ''
+    },
+    // 주차 OId 설정
+    setWeekOIdOpened: (state, action: PayloadAction<string>) => {
+      state.weekOIdOpened = action.payload
+    },
+    // ::
+    // 주차 배열 설정
     setWeekRowArr: (state, action: PayloadAction<ST.WeekRowType[]>) => {
       state.weekRowArr = action.payload
     }
