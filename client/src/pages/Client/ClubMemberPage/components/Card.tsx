582822cbef01028279ca51c87d9453e44bd89294
unknown <dudgns113@gmail.com>
2025-10-21 03:23:12 +0900

    25.10.21 [Client] 멤버 페이지: 카드 스킬 설정 기능 추가
    :white_large_square: [Client] 클럽 멤버 페이지
      - :white_large_square: 코멘트에 대전 정보 표기
      - :construction: 덱 구성 기능
        - :heavy_plus_sign: 카드 스킬 설정창 완성
    
    :white_large_square: [Client] 메인 페이지
    :white_large_square: [Client] 전체 멤버 페이지
      - :white_large_square: 공동체 멤버 수정 기능

--- client/src/pages/Client/ClubMemberPage/components/Card.tsx
+++ client/src/pages/Client/ClubMemberPage/components/Card.tsx
@@ -1,6 +1,7 @@
 import {useCallback, useEffect, useState} from 'react'
 
 import {positionValue} from '@bases/values/positionValues'
+import {batterSkillEntire, batterSkillHero, batterSkillLegend, batterSkillPlatinum, pitcherSkillEntire} from '@bases/values/skillNames'
 
 import type {ChangeEvent, FC} from 'react'
 import type {DivCommonProps} from '@prop'
@@ -13,8 +14,12 @@ type CardProps = DivCommonProps & {card: CardType}
 export const Card: FC<CardProps> = ({card, className, style, ...props}) => {
   const [cardName, setCardName] = useState<string>('')
   const [cardNumStr, setCardNumStr] = useState<string>('')
+  const [skillIdxs, setSkillIdxs] = useState<number[]>([0, 1, 2])
+  const [skillLevels, setSkillLevels] = useState<number[]>([0, 0, 0])
 
   const {posIdx} = card
+
+  const skillNames = posIdx < 11 ? pitcherSkillEntire : batterSkillEntire
 
   const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setCardName(e.currentTarget.value)
@@ -24,10 +29,34 @@ export const Card: FC<CardProps> = ({card, className, style, ...props}) => {
     setCardNumStr(e.currentTarget.value)
   }, [])
 
+  const onChangeSkillIdx = useCallback(
+    (idx: number) => (e: ChangeEvent<HTMLSelectElement>) => {
+      setSkillIdxs(prev => {
+        const newSkillIdxs = [...prev]
+        newSkillIdxs[idx] = parseInt(e.currentTarget.value)
+        return newSkillIdxs
+      })
+    },
+    []
+  )
+
+  const onChangeSkillLevel = useCallback(
+    (idx: number) => (e: ChangeEvent<HTMLSelectElement>) => {
+      setSkillLevels(prev => {
+        const newSkillLevels = [...prev]
+        newSkillLevels[idx] = parseInt(e.currentTarget.value)
+        return newSkillLevels
+      })
+    },
+    []
+  )
+
   // 초기화: 카드의 정보
   useEffect(() => {
     setCardName(card.cardName || '')
     setCardNumStr(card.cardNumber?.toString() || '')
+    setSkillIdxs(card.skillIdxs)
+    setSkillLevels(card.skillLevels)
   }, [card])
 
   return (
@@ -54,17 +83,43 @@ export const Card: FC<CardProps> = ({card, className, style, ...props}) => {
 
       {/* 4. 스킬 정보 */}
       {Array.from({length: 3}, (_, idx) => {
+        const otherIdx1 = (idx + 1) % 3
+        const otherIdx2 = (idx + 2) % 3
+
+        const sIdx = skillIdxs[idx]
+
+        const isLegend = sIdx < batterSkillLegend.length
+        const isPlatinum = !isLegend && sIdx < batterSkillLegend.length + batterSkillPlatinum.length
+        const isHero = !isLegend && !isPlatinum && sIdx < batterSkillLegend.length + batterSkillPlatinum.length + batterSkillHero.length
+
+        const cnBg = isLegend ? '_opt_legend' : isPlatinum ? '_opt_platinum' : isHero ? '_opt_hero' : '_opt_normal'
+
         return (
           <div className="_card_skill_row" key={idx}>
             {/* 4-1. 스킬 이름 */}
-            <select className="_card_skill_name">
-              <option value={0}>닥터 K</option>
-              <option value={1}>라패</option>
-              <option value={2}>슈퍼스타</option>
+            <select className={`_card_skill_name ${cnBg}`} value={skillIdxs[idx]} onChange={onChangeSkillIdx(idx)}>
+              {skillNames.map((skillName, sIdx) => {
+                // 스킬이 겹치면 안된다
+                if (sIdx === skillIdxs[otherIdx1] || sIdx === skillIdxs[otherIdx2]) {
+                  return null
+                }
+
+                const isLegend = sIdx < batterSkillLegend.length
+                const isPlatinum = !isLegend && sIdx < batterSkillLegend.length + batterSkillPlatinum.length
+                const isHero = !isLegend && !isPlatinum && sIdx < batterSkillLegend.length + batterSkillPlatinum.length + batterSkillHero.length
+
+                const cnBg = isLegend ? '_opt_legend' : isPlatinum ? '_opt_platinum' : isHero ? '_opt_hero' : '_opt_normal'
+
+                return (
+                  <option value={sIdx} key={sIdx} className={cnBg}>
+                    {skillName}
+                  </option>
+                )
+              })}
             </select>
 
             {/* 4-2. 스킬 레벨 */}
-            <select className="_card_skill_level">
+            <select className="_card_skill_level" value={skillLevels[idx]} onChange={onChangeSkillLevel(idx)}>
               <option value={0}>E</option>
               <option value={1}>D</option>
               <option value={2}>C</option>
