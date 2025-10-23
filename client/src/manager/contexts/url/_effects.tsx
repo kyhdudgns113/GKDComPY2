f1c32741d264cd0a13135a4527ded7aa1357e199
unknown <dudgns113@gmail.com>
2025-10-24 04:01:04 +0900

    25.10.24 [Client] Template 스크롤 문제 / 멤버 페이지 이동 문제 해결
    :white_check_mark: Template 스크롤시 Lefter 사라지는거 수정
    :bug: 멤버 페이지에서 멤버 선택후 다른 클럽 멤버 페이지로 이동시, 멤버 선택 해제 안되는것 수정

--- client/src/manager/contexts/url/_effects.tsx
+++ client/src/manager/contexts/url/_effects.tsx
@@ -1,7 +1,7 @@
 import {createContext, useContext, useEffect} from 'react'
 import {useLocation} from 'react-router-dom'
 
-import {useAppDispatch, useCommunityStates, useClubActions, useRecordActions} from '@store'
+import {useAppDispatch, useCommunityStates, useClubActions, useMemberActions, useRecordActions} from '@store'
 
 import type {FC, PropsWithChildren} from 'react'
 
@@ -15,11 +15,13 @@ export const useUrlEffectsContext = () => useContext(UrlEffectsContext)
 export const UrlEffectsProvider: FC<PropsWithChildren> = ({children}) => {
   const {banClub, clubArr, community, subClub} = useCommunityStates()
   const {setClubOpened, resetClubOpened} = useClubActions()
+  const {unselectClubMemberOpened} = useMemberActions()
   const {setWeekOIdOpened, resetWeekOIdOpened} = useRecordActions()
 
   const dispatch = useAppDispatch()
   const location = useLocation()
 
+  // URL 바뀔때 clubOpened, weekOIdOpened 설정
   useEffect(() => {
     const url = location.pathname
     let clubOId = ''
@@ -62,6 +64,13 @@ export const UrlEffectsProvider: FC<PropsWithChildren> = ({children}) => {
       dispatch(resetWeekOIdOpened())
     }
   }, [banClub, clubArr, community, subClub, location, dispatch]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  // URL 바뀔때 clubMemberOpened 해제
+  useEffect(() => {
+    return () => {
+      dispatch(unselectClubMemberOpened())
+    }
+  }, [location, dispatch]) // eslint-disable-line react-hooks/exhaustive-deps
   //
   return <UrlEffectsContext.Provider value={{}}>{children}</UrlEffectsContext.Provider>
 }
