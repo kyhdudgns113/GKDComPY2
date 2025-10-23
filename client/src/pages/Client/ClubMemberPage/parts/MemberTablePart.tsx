c03325b87de5328a1795ad22fe8c45410f017e27
unknown <dudgns113@gmail.com>
2025-10-24 03:36:27 +0900

    :white_check_mark: 멤버 리스트 스크롤하면 이것만 바뀌게 수정   - :wrench: 멤버 목록 테이블 스타일 조정 :white_large_square: 클럽 이동 버튼 위치 조정 :white_large_square: Template 스크롤시 Lefter 사라지는거 수정

--- client/src/pages/Client/ClubMemberPage/parts/MemberTablePart.tsx
+++ client/src/pages/Client/ClubMemberPage/parts/MemberTablePart.tsx
@@ -6,14 +6,14 @@ import {useAppDispatch, useMemberActions, useMemberStates} from '@store'
 import {GoldCrown, SilverCrown} from '../components'
 
 import type {FC, MouseEvent} from 'react'
-import type {TableCommonProps} from '@prop'
+import type {DivCommonProps} from '@prop'
 import type {MemberType} from '@shareType'
 
 import * as SV from '@shareValue'
 
 import '../_styles/MemberTablePart.scss'
 
-type MemberTablePartProps = TableCommonProps & {}
+type MemberTablePartProps = DivCommonProps & {}
 
 export const MemberTablePart: FC<MemberTablePartProps> = ({className, style, ...props}) => {
   const {clubMemberArr} = useMemberStates()
@@ -64,58 +64,67 @@ export const MemberTablePart: FC<MemberTablePartProps> = ({className, style, ...
   )
 
   return (
-    <table className={`MemberTable_Part ${className || ''}`} style={style} {...props}>
-      <thead>
-        <tr className="tr_head">
-          <th className="_th_crown"></th>
-          <th className="_th_name" onClick={onClickName} onMouseDown={e => e.preventDefault()}>
-            멤버 이름
-          </th>
-          <th className="_th_batterPower" onClick={onClickBatter} onMouseDown={e => e.preventDefault()}>
-            타자
-          </th>
-          <th className="_th_pitcherPower" onClick={onClickPitcher} onMouseDown={e => e.preventDefault()}>
-            투수
-          </th>
-          <th className="_th_info" onClick={onClickTotal} onMouseDown={e => e.preventDefault()}>
-            -
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {clubMemberArr.map((member, memIdx) => {
-          const isBottomBlue = memIdx % 10 === 9
-          const isBottomGreen = !isBottomBlue && memIdx % 5 === 4
+    <div className={`MemberTable_Part ${className || ''}`} style={style} {...props}>
+      <table>
+        <thead>
+          <tr className="tr_head">
+            <th className="_th_crown"></th>
+            <th className="_th_name" onClick={onClickName} onMouseDown={e => e.preventDefault()}>
+              멤버 이름
+            </th>
+            <th className="_th_batterPower" onClick={onClickBatter} onMouseDown={e => e.preventDefault()}>
+              타자
+            </th>
+            <th className="_th_pitcherPower" onClick={onClickPitcher} onMouseDown={e => e.preventDefault()}>
+              투수
+            </th>
+            <th className="_th_info" onClick={onClickTotal} onMouseDown={e => e.preventDefault()}>
+              -
+            </th>
+          </tr>
+          <tr className="tr_head_shadow">
+            <th className="_th_crown"></th>
+            <th className="_th_name"></th>
+            <th className="_th_batterPower"></th>
+            <th className="_th_pitcherPower"></th>
+            <th className="_th_info"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {clubMemberArr.map((member, memIdx) => {
+            const isBottomBlue = memIdx % 10 === 9
+            const isBottomGreen = !isBottomBlue && memIdx % 5 === 4
 
-          return (
-            <tr
-              className={`tr_body ${isBottomBlue ? '__blue' : isBottomGreen ? '__green' : ''}`}
-              key={memIdx}
-              onClick={onClickMember(member)} // ::
-            >
-              <td className={`_td_crown `}>
-                {member.position === SV.MEMBER_POSITION_GOLD ? (
-                  <GoldCrown />
-                ) : member.position === SV.MEMBER_POSITION_SILVER ? (
-                  <SilverCrown />
-                ) : (
-                  <></>
-                )}
-              </td>
-              <td className="_td_name">{member.memName}</td>
-              <td className="_td_batterPower">{member.batterPower.toLocaleString()}</td>
-              <td className="_td_pitcherPower">{member.pitcherPower.toLocaleString()}</td>
-              <td className="_td_info">
-                {member.memberComment.length > 0 ? (
-                  <IconFilled className="_icon_info" iconName="info" />
-                ) : (
-                  <Icon className="_icon_info" iconName="info" />
-                )}
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                className={`tr_body ${isBottomBlue ? '__blue' : isBottomGreen ? '__green' : ''}`}
+                key={memIdx}
+                onClick={onClickMember(member)} // ::
+              >
+                <td className={`_td_crown `}>
+                  {member.position === SV.MEMBER_POSITION_GOLD ? (
+                    <GoldCrown />
+                  ) : member.position === SV.MEMBER_POSITION_SILVER ? (
+                    <SilverCrown />
+                  ) : (
+                    <></>
+                  )}
+                </td>
+                <td className="_td_name">{member.memName}</td>
+                <td className="_td_batterPower">{member.batterPower.toLocaleString()}</td>
+                <td className="_td_pitcherPower">{member.pitcherPower.toLocaleString()}</td>
+                <td className="_td_info">
+                  {member.memberComment.length > 0 ? (
+                    <IconFilled className="_icon_info" iconName="info" />
+                  ) : (
+                    <Icon className="_icon_info" iconName="info" />
+                  )}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
